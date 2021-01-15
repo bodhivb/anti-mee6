42844ac891d01dac49e1ddd027d272379ffb48b2
GiraffeSummer <gidoselten@hotmail.com>
2021-01-15 12:33:53 +0100

    removed setup bat file

--- gitsetup.bat
+++
@@ -1,7 +0,0 @@
-echo "# anti-mee6" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M production
-git remote add origin https://github.com/Bodhivb/anti-mee6.git
-git push -u origin production
