3cee0708e3176d9372af6395caa24e555bd5b763
GiraffeSummer <gidoselten@hotmail.com>
2021-01-15 12:31:16 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+require('dotenv').config();
+const Mee6 = "159985870458322944"
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.author.id === Mee6) {
+    msg.react('💩');
+  }
+});
+
+client.login(process.env.TOKEN);
