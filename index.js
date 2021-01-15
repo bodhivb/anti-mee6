47b63c58265beea1f8eb91a60f73556a9c1f100c
GiraffeSummer <gidoselten@hotmail.com>
2021-01-15 22:35:24 +0100

    added status manager

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const statusManager = require('./status-manager')(client);
 require('dotenv').config();
 const Mee6 = "159985870458322944"
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  const statusInterval = setinterval(statusManager.ChangeStatus, .5 * 60 * 60 * 1000)
 });
 
 client.on('message', msg => {
---
+++ libraries/statusManager.js
@@ -0,0 +1,17 @@
+class statusManager {
+    constructor(_bot) {
+        this.bot = _bot;
+        this.status = 0;
+        this.stats = [
+            { text: `MEE6 suffer.`, type: { type: "WATCHING" } },
+            /*{ text: ``, type: { type: "LISTENING" } },
+              { text: ``, type: { type: "PLAYING" } },*/
+        ]
+    }
+    ChangeStatus() {
+        this.status = (this.status < this.stats.length - 1) ? this.status + 1 : 0;
+        let stat = this.stats[this.status];
+        this.bot.user.setActivity(stat.text, stat.type);
+    }
+}
+module.exports = (_bot) => { return new statusManager(_bot); }
