eb86bf8a114ae8e675e2dd834b0276b1cd61686e
Bodhi van Baardewijk <bodhivanbaardewijk@hotmail.com>
2021-01-16 21:33:44 +0100

    Improved help

--- commands/help.js
+++ commands/help.js
@@ -1,10 +1,39 @@
 module.exports.config = {
   name: "help",
-  description: "An help page",
+  description:
+    "Show you all of the commands. With {command} you can search for a specific command",
   usage: "help {command}",
 };
 
 module.exports.run = async (bot, message, args) => {
+  //Specific command
+  if (args[0] && args[0] !== "") {
+    const command = bot.commands.get(args[0]);
+    if (command) {
+      return message.channel.send({
+        embed: {
+          title: command.config.name + " command",
+          color: 3447003,
+          fields: [
+            { name: "Description", value: command.config.description, inline: true },
+            { name: "Usage", value: "`" + command.config.usage + "`", inline: true },
+          ],
+        },
+      });
+    } else {
+      return message.channel.send({
+        embed: {
+          color: 15105570,
+          description:
+            "Sorry, " +
+            args[0] +
+            " does not exist. You can see which commands are available via `help`",
+        },
+      });
+    }
+  }
+
+  //All commands
   let helpMessages = [];
   bot.commands.forEach((command) => {
     helpMessages.push({
@@ -13,10 +42,13 @@ module.exports.run = async (bot, message, args) => {
     });
   });
 
-  message.channel.send({
+  message.author.send({
     embed: {
-      title: "Help",
+      title: "Available commands",
+      color: 12901133,
       fields: helpMessages,
     },
   });
+
+  message.channel.send("Help pages has been sent to you");
 };
