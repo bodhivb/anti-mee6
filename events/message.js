21b43619164bfedd23283c0e6369300b00b960ef
GiraffeSummer <gidoselten@hotmail.com>
2021-01-16 13:22:13 +0100

    renamed mee6 var to full caps, ignoring webhooks now too

--- events/message.js
+++ events/message.js
@@ -1,16 +1,17 @@
-const Mee6 = "159985870458322944";
+const MEE6 = "159985870458322944";
 
 // Handling an incoming message
 module.exports = async (bot, message) => {
   //Check if message is from MEE6 bot?
-  if (message.author.id === Mee6) {
+  if (message.author.id === MEE6) {
     message.react(Math.round(Math.random()) >= 1 ? "💩" : "🤮");
   }
+  if (message.author.bot || message.webhookID) return; //if bot or webhook skip
 
   const prefix = "<@!" + bot.user.id + ">";
 
   //TODO: Make check mention function
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
 
   message.react("😈");
   const args = message.content.slice(prefix.length).split(" ");
