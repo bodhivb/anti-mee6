b2f9e47b1a0af52969c5e56a496fa56276fb42aa
Bodhi van Baardewijk <bodhivanbaardewijk@hotmail.com>
2021-01-16 12:57:42 +0100

    Fixed message prefix

--- events/message.js
+++ events/message.js
@@ -1,4 +1,3 @@
-const prefix = ".";
 const Mee6 = "159985870458322944";
 
 // Handling an incoming message
@@ -8,18 +7,16 @@ module.exports = async (bot, message) => {
     message.react(Math.round(Math.random()) >= 1 ? "💩" : "🤮");
   }
 
+  const prefix = "<@!" + bot.user.id + ">";
+
   //TODO: Make check mention function
-  if (
-    (!message.content.startsWith(prefix) &&
-      !message.content.startsWith("<@!" + bot.user.id + ">")) ||
-    message.author.bot
-  )
-    return;
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
 
   message.react("😈");
-  const args = message.content.slice(prefix.length + 1).split(" ");
-  const commandName = args.shift().toLowerCase();
+  const args = message.content.slice(prefix.length).split(" ");
+  if (args[0] === "") args.shift();
 
+  const commandName = args.shift().toLowerCase();
   const command = bot.commands.get(commandName);
   if (command) command.run(bot, message, args);
 };
