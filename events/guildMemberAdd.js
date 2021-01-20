1fe78ab03ac5486308582cfc3a858531388582a3
Bodhi van Baardewijk <bodhivanbaardewijk@hotmail.com>
2021-01-20 12:32:00 +0100

    Fixed add/remove member

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,21 +2,8 @@
 const MEE6 = "159985870458322944";
 const guildID = "799590946475999282";
 const HateroleID = "799593265178738788";
-module.exports = async (bot, memberJoin) => {
-    memberJoin = JSON.parse(JSON.stringify(memberJoin));
-    console.log("1")
-    if (memberJoin.guildID == guildID) {
-        console.log("2")
-        const guild = await bot.guilds.cache.get(memberJoin.guildID);
-        const member = await guild.members.cache.get(MEE6);
-        console.log("3")
-        //if MEE6
-        console.log(JSON.stringify(memberJoin))
-        console.log(JSON.stringify(member))
-       // if (member.user.id == MEE6) {
-            const guildMember = await guild.members.cache.get(MEE6);
-            guildMember.roles.set([HateroleID]);
-            console.log("4")
-       // }
-    }
+module.exports = async (bot, member) => {
+  const guild = member.guild;
+
+  if (guild.id === guildID && member.id === MEE6) member.roles.set([HateroleID]);
 };
--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -3,9 +3,9 @@ const guildID = "799590946475999282"
 const channelId = "799623827930742784"
 const inviteUrl = "https://discord.com/oauth2/authorize?scope=bot&response_type=code&client_id=159985415099514880&guild_id=799590946475999282"
 module.exports = async (bot, member) => {
-    //if MEE6
-    if (member.user.id = "159985870458322944" && member.guild.id == guildID) {
-        const channel = member.guild.channels.cache.get(channelId);
-        channel.send(`@here, MEE6 Was kicked, invite again! \n${inviteUrl}`)
-    }
+  //if MEE6
+  if (member.user.id == "159985870458322944" && member.guild.id == guildID) {
+    const channel = member.guild.channels.cache.get(channelId);
+    channel.send(`@ developer, MEE6 Was kicked, invite again! \n${inviteUrl}`);
+  }
 };
