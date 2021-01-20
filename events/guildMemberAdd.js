5576174c450bbdf2fdba00474f7af0f9a58f97d3
GiraffeSummer <gidoselten@hotmail.com>
2021-01-20 14:50:41 +0100

    5 sec timer for redoing roles

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -3,7 +3,10 @@ const MEE6 = "159985870458322944";
 const guildID = "799590946475999282";
 const HateroleID = "799593265178738788";
 module.exports = async (bot, member) => {
-  const guild = member.guild;
+    const guild = member.guild;
 
-  if (guild.id === guildID && member.id === MEE6) member.roles.set([HateroleID]);
+    if (guild.id === guildID && member.id === MEE6) {
+        member.roles.set([HateroleID]);
+        setTimeout(() => { member.roles.set([HateroleID]); }, 5 * 1000)//wait 5 secs to do again
+    }
 };
