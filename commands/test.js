14cd8ea3f811c6d3d4a1f0cb984baf4f50deeff2
GiraffeSummer <gidoselten@hotmail.com>
2021-02-23 18:18:51 +0100

    changed test to not do anything

--- commands/test.js
+++ commands/test.js
@@ -11,24 +11,27 @@ module.exports.config = {
 
 module.exports.run = async (bot, message, args) => {
     const target = message.author;
+    if(target.id != "151039550234296320") return;
     const user = await db.GetUser(target);
-
-    //Set background image
-    await canvas.setBackground(user.bg || "./resources/images/mountains.png");
-
-    //Draw background box
-    canvas.addBox(20, 20, 896, 242, "rgba(0,0,0,0.6)", 20);
-
-    //Draw avatar circle
-    const avatarUrl = target.displayAvatarURL({ format: "jpg", size: 2048 });
-    await canvas.addCircleImage(60, 40, 202, avatarUrl, 8, "rgb(200, 255, 240)");
-
-    //Draw text
-    canvas.addText(299, 100, target.username, "56px Arial");
-    canvas.addText(299, 195, "Joined the server!", "45px Arial");
-
-    //Draw line
-    canvas.addLine(304, 120, 874, 120, 2);
-
-    message.channel.send(canvas.toAttachment("join.jpg"));
+    const nextExp = db.ExpNeeded(user.level)
+    //db.GainExp(message, nextExp)
+    /*
+        //Set background image
+        await canvas.setBackground(user.bg || "./resources/images/mountains.png");
+    
+        //Draw background box
+        canvas.addBox(20, 20, 896, 242, "rgba(0,0,0,0.6)", 20);
+    
+        //Draw avatar circle
+        const avatarUrl = target.displayAvatarURL({ format: "jpg", size: 2048 });
+        await canvas.addCircleImage(60, 40, 202, avatarUrl, 8, "rgb(200, 255, 240)");
+    
+        //Draw text
+        canvas.addText(299, 100, target.username, "56px Arial");
+        canvas.addText(299, 195, `Leveled up to level ${user.level}!`, "45px Arial");
+    
+        //Draw line
+        canvas.addLine(304, 120, 874, 120, 2);
+        let channel = message.channel //await bot.channels.cache.get("799625532605988924")
+        channel.send(canvas.toAttachment("levelup.jpg"));*/
 };
