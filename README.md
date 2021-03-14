cef6ef3cfdb84e6d1a7a05756860e4641089aaa3
GiraffeSummer <gidoselten@hotmail.com>
2021-03-14 13:38:37 +0100

    template commands test

--- README.md
+++ README.md
@@ -22,4 +22,52 @@ No working? Or do you have any questions? You can always ask on the discord serv
 
 ## Commands
 
-Coming soon!
+#### background  
+change background image  
+Usage: `@Anti-MEE6#5212 background {image url}`  
+#### balance  
+Get dungeon balance  
+Usage: `@Anti-MEE6#5212 balance`  
+#### buy  
+Buy item from shop  
+Usage: `@Anti-MEE6#5212 buy`  
+#### daily  
+Daily reward  
+Usage: `@Anti-MEE6#5212 daily`  
+#### inventory  
+View inventory  
+Usage: `@Anti-MEE6#5212 inventory`  
+#### item  
+view specific item  
+Usage: `@Anti-MEE6#5212 item {name}`  
+#### shop  
+Shop dungeon items  
+Usage: `@Anti-MEE6#5212 shop`  
+#### help  
+Show you all of the commands. With {command} you can search for a specific command  
+Usage: `@Anti-MEE6#5212 help {command}`  
+#### idea  
+Add your idea to our trello board  
+Usage: `@Anti-MEE6#5212 idea {idea}`  
+#### invite  
+Invite the bot to your server  
+Usage: `@Anti-MEE6#5212 invite`  
+#### join  
+Join discord server  
+Usage: `@Anti-MEE6#5212 join`  
+#### lvldata  
+Test message to show exp and level  
+Usage: `@Anti-MEE6#5212 lvldata`  
+#### nickname  
+Change the nickname of MEE6  
+Usage: `@Anti-MEE6#5212 nickname [nickname]`  
+#### ping  
+ping bot  
+Usage: `@Anti-MEE6#5212 ping`  
+#### rank  
+Show your rank status  
+Usage: `@Anti-MEE6#5212 rank`  
+#### test  
+test  
+Usage: `@Anti-MEE6#5212 test`  
+
--- generateDocs.js
+++ generateDocs.js
@@ -5,23 +5,34 @@ const func = require("./libraries/functions");
 
 //run when launched, add Functions calls here if more needs to be added.
 (async () => {
-    const text = await GenerateCommandList();
-    fs.writeFileSync("./commands.md", text);
+    await GenerateReadme();
 
 
     process.exit(0);
 })()
 
+async function GenerateReadme() {
+    return new Promise(async resolve => {
+        const commands = await GenerateCommandList();
+        const template = await fs.promises.readFile("./resources/readme-template.md")
+        const text = template.toString().replace("<_COMMANDS>", commands);
+        fs.writeFileSync("./README.md", text);
+
+
+        resolve();
+    })
+}
+
 
 function GenerateCommandList() {
     return new Promise(resolve => {
-        let text = "## Commands\n\n"
+        let text = ""
 
         fs.readdirSync("./commands/").forEach((file) => {
             //Only js code may be loaded
             if (!file.endsWith(".js")) return;
             let command = require(`./commands/${file}`).config;
-            command = func.MakeValid(command , func.baseCommand);
+            command = func.MakeValid(command, func.baseCommand);
             if (command.enabled && !command.admin) {
                 console.log(`Command found: ${command.name} (${file})`);
                 text += `#### ${command.name}  \n${command.description}  \nUsage: \`@Anti-MEE6#5212 ${command.usage}\`  \n`
